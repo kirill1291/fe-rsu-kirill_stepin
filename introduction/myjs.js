7333f6a6a100b458676aabc85048005648b02320
Kirill Stepin <Kirill1291@gmail.com>
2016-10-04 13:29:13 +0300

    Added introduction

---
+++ introduction/myjs.js
@@ -0,0 +1,16 @@
+﻿ "use strict";
+var fname = "Kirill";
+var lname = "Stepin";
+var age = "25";
+alert(fname);
+alert(lname);
+alert(age);
+function myFunction() {
+//получаем идентификатор элемента
+    var a = document.getElementById("test");
+//вешаем на него событие
+    a.onclick = function () {
+alert("Зачем нажал А???");
+    };
+}
+window.onload = myFunction;
